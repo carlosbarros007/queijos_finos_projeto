8739f97e2788ee8664518267588fcc699d7a35ce
carlosbarros007 <carlosbarros723513@gmail.com>
2024-03-10 16:04:40 -0300

    iniciando o projeto com as dependencias iniciais

---
+++ src/main/java/com/projeto_queijos_finos/projeto_queijos_finos/ProjetoQueijosFinosApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_queijos_finos.projeto_queijos_finos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoQueijosFinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoQueijosFinosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_queijos_finos/projeto_queijos_finos/ProjetoQueijosFinosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_queijos_finos.projeto_queijos_finos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoQueijosFinosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
